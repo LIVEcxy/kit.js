348b68fd77e1b182efffa70728c797ca78464117
liusm <simin@nfschina.com>
2021-11-30 15:21:51 +0800

    修改构建配置和出口配置

--- script/kit.js
+++ script/kit.js
@@ -22,145 +22,156 @@
 
 const kit = {
 
-    version: '1.0.7',
-    author : 'liusm',
+  version: '1.0.8',
+  author: 'liusm',
 
-    isEmpty(value = '') {
-        if (typeof value === 'object') {
-            return this.isEmptyObj(value);
-        }
-        return ('' + value) === '' ? true : false;
-    },
-    isEmptyObj(obj = '') {
-        if (typeof obj !== 'object') {
-            throw `isEmptyObj 's params not Object or Array`;
-        } else {
-            let name;
-            for (name in obj) return false;
-            return true;
-        }
-    },
-    getType(value = undefined) {
-        const types = Object.prototype.toString.call(value);
-
-        if (types) return types.slice(8, -1);
-        throw `unknown type: ${types},value: ${value}`;
-    },
-    getQuery(key = '') {
-        const search = location.search.includes('?') ? location.search.slice(1) : false;
-
-        if(search){
-            let query = {};
-            let searchArr = search.split('&');
-
-            searchArr.map(item => {
-                query[item.split('=')[0]] = unescape(item.split('=')[1])
-            })
-
-            let result = key ? query[key] : query;
-            return result;
-        }
-    },
-    getTimes(d = new Date()) {
-        return Math.round(d / 1000);
-    },
-    getNumber(value = 0) {
-        return Math.round(value * 100) / 100;
-    },
-    toBoolean(value) {
-        return Boolean(value);
-    },
-    toString(value = '') {
-        let result = '';
-        let vType = this.getType(value);
-
-        switch (vType) {
-        case 'Object ':
-            result = JSON.stringify(value);
-            break;
-        default:
-            result = String(value);
-        }
-        return result;
-    },
-    toNumber(value = 0) {
-        let result = '';
-        let vType = this.getType(value);
-
-        switch (vType) {
-        case 'String ':
-            result = Number(value);
-            break;
-        case 'Boolean ':
-            result = Number(value);
-            break;
-        default:
-            console.warn('params is not a convertible ');
-            result = value;
-        }
-        return result;
-    },
-    toArray(value = '') {
-        let result = '';
-        let vType = this.getType(value);
-
-        switch (vType) {
-        case 'Null ':
-            result = value;
-            break;
-        case 'Number ':
-            result = Array.from(this.toString(value));
-            break;
-        case 'Object ':
-            let _array = [];
-            for (let item in value) {
-                _array.push({
-                    'key ': item,
-                    'value ': value[item]
-                });
-            }
-            result = _array;
-            break;
-        default:
-            result = Array.from(value);
-        }
-        return result;
-    },
-    toObject(value = '') {
-        let result = '';
-        let vType = this.getType(value);
-
-        switch (vType) {
-        case 'Null ':
-            result = value;
-            break;
-        case 'Number ':
-            try {
-                JSON.parse(this.toString(value));
-            } catch (err) {
-                console.warn('params is not a convertible ');
-                result = value;
-            }
-            break;
-        case 'String ':
-            try {
-                JSON.parse(value);
-            } catch (err) {
-                console.warn('params is not a convertible ');
-                result = value;
-            }
-            break;
-        case 'Array ':
-            let _object = {};
-            for (let [index, item] of value.entries()) {
-                _object[index] = item
-            }
-            result = _object;
-            break;
-        default:
-        }
-        return result;
+  isEmpty (value = '') {
+    if (typeof value === 'object') {
+      return this.isEmptyObj(value);
     }
+    return ('' + value) === '' ? true : false;
+  },
+  isEmptyObj (obj = '') {
+    if (typeof obj !== 'object') {
+      throw `isEmptyObj 's params not Object or Array`;
+    } else {
+      let name;
+      for (name in obj) return false;
+      return true;
+    }
+  },
+  getType (value = undefined) {
+    const types = Object.prototype.toString.call(value);
+
+    if (types) return types.slice(8, -1);
+    throw `unknown type: ${types},value: ${value}`;
+  },
+  getQuery (key = '') {
+    const search = location.search.includes('?') ? location.search.slice(1) : false;
+
+    if (search) {
+      let query = {};
+      let searchArr = search.split('&');
+
+      searchArr.map(item => {
+        query[item.split('=')[0]] = unescape(item.split('=')[1])
+      })
+
+      let result = key ? query[key] : query;
+      return result;
+    }
+  },
+  getTimes (d = new Date()) {
+    return Math.round(d / 1000);
+  },
+  getNumber (value = 0) {
+    return Math.round(value * 100) / 100;
+  },
+  delete (target = '', key) {
+    if (this.getType(target) === 'Object') {
+      delete target[key]
+      return target;
+    }
+    if (this.getType(target) === 'Array') {
+      target.splice(key, 1)
+      return target;
+    }
+    return target = null;
+  },
+  toBoolean (value) {
+    return Boolean(value);
+  },
+  toString (value = '') {
+    let result = '';
+    let vType = this.getType(value);
+
+    switch (vType) {
+      case 'Object ':
+        result = JSON.stringify(value);
+        break;
+      default:
+        result = String(value);
+    }
+    return result;
+  },
+  toNumber (value = 0) {
+    let result = '';
+    let vType = this.getType(value);
+
+    switch (vType) {
+      case 'String ':
+        result = Number(value);
+        break;
+      case 'Boolean ':
+        result = Number(value);
+        break;
+      default:
+        console.warn('params is not a convertible ');
+        result = value;
+    }
+    return result;
+  },
+  toArray (value = '') {
+    let result = '';
+    let vType = this.getType(value);
+
+    switch (vType) {
+      case 'Null ':
+        result = value;
+        break;
+      case 'Number ':
+        result = Array.from(this.toString(value));
+        break;
+      case 'Object ':
+        let _array = [];
+        for (let item in value) {
+          _array.push({
+            'key ': item,
+            'value ': value[item]
+          });
+        }
+        result = _array;
+        break;
+      default:
+        result = Array.from(value);
+    }
+    return result;
+  },
+  toObject (value = '') {
+    let result = '';
+    let vType = this.getType(value);
+
+    switch (vType) {
+      case 'Null ':
+        result = value;
+        break;
+      case 'Number ':
+        try {
+          JSON.parse(this.toString(value));
+        } catch (err) {
+          console.warn('params is not a convertible ');
+          result = value;
+        }
+        break;
+      case 'String ':
+        try {
+          JSON.parse(value);
+        } catch (err) {
+          console.warn('params is not a convertible ');
+          result = value;
+        }
+        break;
+      case 'Array ':
+        let _object = {};
+        for (let [index, item] of value.entries()) {
+          _object[index] = item
+        }
+        result = _object;
+        break;
+      default:
+    }
+    return result;
+  }
 };
 
 export { kit };
--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,38 @@
  * @Autor: liusm
  * @Date: 2020-04-02 12:48:38
  * @LastEditors: liusm
- * @LastEditTime: 2020-04-02 12:48:59
+ * @LastEditTime: 2021-11-30 15:16:18
  */
 
 const path = require('path');
-const UglifyJsPlugin  = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 
 const config = {
-    entry: {
-        kit: './kit.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins:[
-        new UglifyJsPlugin()
+  entry: {
+    kit: '/script/kit.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 }
 
 module.exports = config;
